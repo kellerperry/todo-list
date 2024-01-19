fe0e13e2fdb88db7b9129819317d6b6acf738523
Keller Perry <130608533+kellerperry@users.noreply.github.com>
2024-01-19 12:03:24 -0700

    Create Task factory

---
+++ src/components/task.js
@@ -0,0 +1,12 @@
+export default function Task (name, description, dueDate, priority, project) {
+    name;
+    description;
+    dueDate;
+    priority;
+    project;
+    return { name, description, dueDate, priority, project }
+}
+
+
+
+
