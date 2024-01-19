ca7060ed18cee29a075c1c7ac08c6ef15957a662
Keller Perry <130608533+kellerperry@users.noreply.github.com>
2024-01-19 12:21:46 -0700

    Fix Task factory methods

--- src/components/task.js
+++ src/components/task.js
@@ -1,12 +1,41 @@
-export default function Task (name, description, dueDate, priority, project) {
+function Task (name) {
     name;
-    description;
-    dueDate;
-    priority;
-    project;
-    return { name, description, dueDate, priority, project }
-}
+    
+    const addDescription = (description) => {
+        this.description = description;
+    }
+    
+    const getDescription = () => {
+        return this.description;
+    }
 
+    const setDueDate = (dueDate) => {
+        this.dueDate = dueDate;
+    }
+
+    const getDueDate = () => {
+        return this.dueDate;
+    }
+
+    const setPriority = (priority) => {
+        this.priority = priority;
+    }
+
+    const getPriority = () => {
+        return this.priority;
+    }
+    
+    const addProject = (project) => {
+        this.project = project;
+    }
+
+    const getProject = () => {
+        return this.project;
+    }
+
+    
+    return { name, addDescription, getDescription, setPriority, addProject, getProject, getPriority, setDueDate, getDueDate }
+}
 
 
 
